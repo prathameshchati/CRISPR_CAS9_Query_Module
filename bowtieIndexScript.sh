6234208d6e804524f62af06b271bfd4466eb71ec
prathameshchati <chati.p@wustl.edu>
2022-05-31 15:37:41 -0500

    cctop script

---
+++ bowtieIndexScript.sh
@@ -0,0 +1,1 @@
+bowtie-build -r -f AAVS1_Sequence_FASTA.fasta AAVS1_Bowtie_Index
---
+++ cctopScript.sh
@@ -0,0 +1,1 @@
+cctop --input AAVS1_Sequence_FASTA.fasta --index AAVS1_Bowtie_Index --pam NGG --totalMM 5 --coreLength 12 --coreMM 2 --output CCTop_AAVS1_Results
